512225942559810b8940515a27c20a2d9d156cdf
Heri Kurniawan <herikurniawan@M1.local>
2026-06-15 20:27:15 +0700

    remove logo animation

--- src/components/FidzDevLogo.tsx
+++ src/components/FidzDevLogo.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { motion } from "motion/react";
 
 interface FidzDevLogoProps {
   className?: string;
@@ -10,16 +9,10 @@ export default function FidzDevLogo({
   className = "h-10 w-10 md:h-12 md:w-12",
 }: FidzDevLogoProps) {
   return (
-    <motion.img
-  src="/logo.png"
-  alt="FidzDev Logo"
-  className={`${className} object-contain scale-[1.1] origin-left`}
-  animate={{ y: [0, -4, 0] }}
-  transition={{
-    duration: 3.5,
-    repeat: Infinity,
-    ease: "easeInOut",
-  }}
-/>
+    <img
+      src="/logo.png"
+      alt="FidzDev Logo"
+      className={`${className} object-contain scale-[1.1] origin-left`}
+    />
   );
 }
